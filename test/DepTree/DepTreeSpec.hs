6550469046e88f7d38ab1a61005f434d787a2567
jwin <jwin1968@gmail.com>
2018-10-15 22:41:50 +0200

    better test

--- test/DepTree/DepTreeSpec.hs
+++ test/DepTree/DepTreeSpec.hs
@@ -23,8 +23,11 @@ depTxtTree =
   [ (0, "com.example:producer:jar:0.0.1-SNAPSHOT")
   , (3, "org.springframework.cloud:spring-cloud-stream:jar:2.0.0.RC3:compile")
   , (6, "org.springframework.boot:spring-boot-starter-validation:jar:2.0.2.RELEASE:compile")
+  , (6, "A")
   , (9, "org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile")
+  , (12, "C")
   , (6, "org.springframework:spring-messaging:jar:5.0.6.RELEASE:compile")
+  , (6, "C")
   , (9, "org.springframework:spring-beans:jar:5.0.6.RELEASE:compile")
   ]
 
@@ -38,12 +41,17 @@ spec = do
     it "all paths (from root) to node '2A'" $ do getPaths "2A" testTree `shouldBe` [["0", "1A", "2A"]]
     it "all paths (from root) to node '3x'" $ do
       getPaths "3x" testTree `shouldBe` [["0", "1B", "3x"], ["0", "1A", "2A", "3x"]]
-    it "get path to node 'org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile'" $ do
-      getPaths "org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile" depTxtTree `shouldBe`
+    it "get path to node 'C'" $ do
+      getPaths "C" depTxtTree `shouldBe`
         [ [ "com.example:producer:jar:0.0.1-SNAPSHOT"
           , "org.springframework.cloud:spring-cloud-stream:jar:2.0.0.RC3:compile"
-          , "org.springframework.boot:spring-boot-starter-validation:jar:2.0.2.RELEASE:compile"
+          , "C"
+          ]
+        , [ "com.example:producer:jar:0.0.1-SNAPSHOT"
+          , "org.springframework.cloud:spring-cloud-stream:jar:2.0.0.RC3:compile"
+          , "A"
           , "org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile"
+          , "C"
           ]
         ]
     it "no paths" $ do getPaths "42" testTree `shouldBe` []
