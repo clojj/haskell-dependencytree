5cde2dfb9c864fffd437710848243b28fb19ef13
jwin <jwin1968@gmail.com>
2018-10-15 22:28:28 +0200

    read dep.txt and convert to Tree add tests

--- src/DepTree.hs
+++ src/DepTree.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module DepTree
   ( getPaths
+  , toTree
   , Tree
   ) where
 
-import           Data.List (findIndex, findIndices, foldl, foldl', foldr, map,
-                            reverse, take)
+import Data.List (findIndex, findIndices, foldl, foldl', foldr, map, reverse, take)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
-type Name = String
+import Data.Char (isAlpha)
 
-type TreeItem = (Int, String)
+type Name = T.Text
+
+type TreeItem = (Int, T.Text)
 
 type Tree = [TreeItem]
 
@@ -18,7 +24,7 @@ findItem name tree = findIndices (\e -> (snd e) == name) tree
 treesToItem :: Name -> Tree -> [Tree]
 treesToItem name tree = do
   case findItem name tree of
-    []      -> []
+    [] -> []
     indexes -> foldl' (\list n -> (take (n + 1) tree) : list) [] indexes
 
 rootPath :: Tree -> [Name]
@@ -33,3 +39,14 @@ getPaths :: Name -> Tree -> [[Name]]
 getPaths name tree = do
   let trees = treesToItem name tree
    in map (rootPath . reverse) trees
+
+toTree :: FilePath -> IO Tree
+toTree file = do
+  content <- TIO.readFile file
+  let lines = T.lines content
+   in return $ map lineToTreeItem lines
+
+lineToTreeItem :: T.Text -> TreeItem
+lineToTreeItem line =
+  let (pre, post) = T.break isAlpha line
+   in (T.length pre, post)
--- test/DepTree/DepTreeSpec.hs
+++ test/DepTree/DepTreeSpec.hs
@@ -1,4 +1,9 @@
-module DepTree.DepTreeSpec (main, spec) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module DepTree.DepTreeSpec
+  ( main
+  , spec
+  ) where
 
 import Test.Hspec
 import Test.QuickCheck
@@ -13,12 +18,32 @@ main = hspec spec
 testTree :: Tree
 testTree = [(0, "0"), (1, "1A"), (2, "2A"), (3, "3x"), (1, "1B"), (2, "3x")]
 
+depTxtTree :: Tree
+depTxtTree =
+  [ (0, "com.example:producer:jar:0.0.1-SNAPSHOT")
+  , (3, "org.springframework.cloud:spring-cloud-stream:jar:2.0.0.RC3:compile")
+  , (6, "org.springframework.boot:spring-boot-starter-validation:jar:2.0.2.RELEASE:compile")
+  , (9, "org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile")
+  , (6, "org.springframework:spring-messaging:jar:5.0.6.RELEASE:compile")
+  , (9, "org.springframework:spring-beans:jar:5.0.6.RELEASE:compile")
+  ]
+
 spec :: Spec
 spec = do
+  describe "file to Tree" $ do
+    it "read file dep.txt and create Tree" $ do
+      tree <- toTree "dep.txt"
+      tree `shouldBe` depTxtTree
   describe "getPath" $ do
-    it "all paths (from root) to the name" $ do
-      getPaths "2A" testTree `shouldBe`  [["0","1A","2A"]]
-    it "all paths (from root) to the name" $ do
-      getPaths "3x" testTree `shouldBe`  [["0","1B","3x"],["0","1A","2A","3x"]]
-    it "no paths" $ do
-      getPaths "42" testTree `shouldBe`  []
+    it "all paths (from root) to node '2A'" $ do getPaths "2A" testTree `shouldBe` [["0", "1A", "2A"]]
+    it "all paths (from root) to node '3x'" $ do
+      getPaths "3x" testTree `shouldBe` [["0", "1B", "3x"], ["0", "1A", "2A", "3x"]]
+    it "get path to node 'org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile'" $ do
+      getPaths "org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile" depTxtTree `shouldBe`
+        [ [ "com.example:producer:jar:0.0.1-SNAPSHOT"
+          , "org.springframework.cloud:spring-cloud-stream:jar:2.0.0.RC3:compile"
+          , "org.springframework.boot:spring-boot-starter-validation:jar:2.0.2.RELEASE:compile"
+          , "org.apache.tomcat.embed:tomcat-embed-el:jar:8.5.31:compile"
+          ]
+        ]
+    it "no paths" $ do getPaths "42" testTree `shouldBe` []
