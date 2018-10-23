ee5d2b4458618afcc063e89e24af85d2fa1ba109
jwin <jwin1968@gmail.com>
2018-10-23 20:34:49 +0200

    separate paths in output

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,24 @@ import           Data.Text     (unpack)
 import           DepTree       (Tree, getPaths)
 
 testTree :: Tree
-testTree = [(0, "0", "0"), (1, "1A", "+- 1A"), (2, "2A", "|  +- 2A"), (3, "3x", "|  |  +- 3x"), (1, "1B", "+- 1B"), (2, "2B", "|  +- 2B"), (3, "3x", "|  |  +- 3x")]
+testTree =
+  [ (0, "0", "0")
+  , (1, "1A", "+- 1A")
+  , (2, "2A", "|  +- 2A")
+  , (3, "3x", "|  |  +- 3x")
+  , (1, "1B", "+- 1B")
+  , (2, "2B", "|  +- 2B")
+  , (3, "3x", "|  |  +- 3x")
+  ]
 
 main :: IO ()
-main = forM_ outputLines putStrLn
+main =
+  forM_
+    outputPaths
+    (\lines -> do
+       putStrLn ""
+       forM_ lines putStrLn)
   where
-    paths = concat $ getPaths "3x" testTree
-    outputLines = [unpack line | (_, _, line) <- paths]
+    paths = getPaths "3x" testTree
+    outputPaths = outputPath <$> paths
+    outputPath resultItems = [unpack line | (_, _, line) <- resultItems]
